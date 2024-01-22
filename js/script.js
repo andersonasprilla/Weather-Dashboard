478e634462908cfa6f26ff6a0acc747c5a1b2b67
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-01-22 12:05:48 -0500

    add five day forecast

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const APIKey = '9ecd32ab11a8da67215f420ae16fd52d';
 
 $(document).ready(function () {
 
-    function displayCard () {
+    function displayCurrentWeatherCard () {
         $('#card').addClass('card mx-2')
-        $('#cityName').addClass('card-header')
+        $('#current-weather-header').addClass('card-header')
         $('#card-body').addClass('card-body')
         $('#ul-list').addClass('list-group list-group-flush')
         $('temp').addClass("list-group-item")
@@ -12,19 +12,40 @@ $(document).ready(function () {
         $('humidity').addClass("list-group-item")
     }
 
-    function getAPI(city) {
-        const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+    function displayFiveDayForecastCard() {
+        for(var i = 0; i < 5; i++){
+            displayCurrentWeatherCard()
+        }
+    }
 
-        fetch(requestUrl)
+    function getAPI(city) {
+        const requestCurrentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+        const requestFiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`
+
+        // Fetch current weather data
+        fetch(requestCurrentWeatherUrl)
             .then(function (response) {
                 return response.json()
             })
-            .then(function (data) {
-                displayCard()
-                $('#cityName').text(data.name + dayjs().format(' MM/DD/YYYY'))
-                $('#temp').text('Temperature: ' + data.main.temp + ' F°')
-                $('#wind').text('Wind: ' + data.wind.speed + ' MPH')
-                $('#humidity').text('Humidity: ' + data.main.humidity + '%')
+            .then(function (currentWeatherData) {
+                displayCurrentWeatherCard()
+                $('#current-weather-header').text(currentWeatherData.name + dayjs().format(' MM/DD/YYYY'))
+                $('#temp').text('Temperature: ' + currentWeatherData.main.temp + ' F°')
+                $('#wind').text('Wind: ' + currentWeatherData.wind.speed + ' MPH')
+                $('#humidity').text('Humidity: ' + currentWeatherData.main.humidity + '%')
+
+                //Fetch five-day forecast data
+                return fetch(requestFiveDayForecastUrl)
+            })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (fiveDayForecastData) {
+                console.log(fiveDayForecastData)
+                $('#five-day-forecast-header').text(dayjs(fiveDayForecastData.list[2].dt_txt).format('MM/DD/YYYY'))
+                $('#five-day-forecast-temp').text('Temperature: '+ fiveDayForecastData.list[2].main.temp + ' F°')
+                $('#five-day-forecast-wind').text('Wind: ' + fiveDayForecastData.list[2].wind.speed + ' MPH')
+                $('#five-day-forecast-humidity').text('Humidity: ' + fiveDayForecastData.list[2].main.humidity + '%')
             })
     }
 
